dab4ec44aa31a1e6fa9b6a8ecbba465ebdde6278
Michelle <clairsin@gmail.com>
2022-12-17 16:56:03 -0500

    completed day05

---
+++ Day05_CandySale.js
@@ -0,0 +1,32 @@
+import products from "./Day05_data.js";
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+console.log(products)
+
+function getSaleItems(data){
+    let candies = data.filter((item) => item.type === "sweet")
+    let shoppingCart = candies.map((item) => {
+      return {
+        item: item.item,
+        price: item.price
+      }
+    })
+    return shoppingCart
+};
+
+console.log(getSaleItems(products))
