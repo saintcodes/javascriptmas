99a78fa821da02214ce3af14cf5337a4721c5233
Michelle <clairsin@gmail.com>
2022-12-18 14:16:12 -0500

    Day10 solution

---
+++ Day10_SortByLength.js
@@ -0,0 +1,26 @@
+function sortByLength(strs) {
+  //  write code here.
+  return strs.sort(function(a, b){return a.length - b.length})
+}
+
+
+const strs = ["abc", "", "aaa", "a", "zz"];
+console.log(sortByLength(strs))
+/**
+* Test Suite 
+*/
+// describe('sortByLength()', () => {
+//   it('sorts the strings from shortest to longest', () => {
+//       // arrange
+//       const strs = ["abc", "", "aaa", "a", "zz"];
+      
+//       // act
+//       const result = sortByLength(strs);
+
+//       // log
+//       console.log("result: ", result);
+      
+//       // assert
+//       expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
+//   });
+// });
